6fe58a83546d894dd613a362a4994c706e749fe4
Biswabhusan Pradhan <biswabhusanpradhan380@gmail.com>
2018-05-01 20:18:31 +0530

    http call with refresh token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { DataService } from '../data.service'
 import { Router } from '@angular/router'
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,19 @@ import { Router } from '@angular/router'
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private http:HttpClient, private _data: DataService, private router:Router) { }
+  constructor(private http:HttpClient, private _data: DataService, private router:Router) { 
+
+    var reqHeader = new HttpHeaders({authorization:'Basic c2thdGVyaWs6cGFzcw=='});
+    let myObservable=Observable.interval(20000);
+
+    myObservable.subscribe(x=>{
+      this.http.post('/oauth/token?grant_type=refresh_token&refresh_token='+this._data.access_key.refresh_token,{"headers":reqHeader})
+      .subscribe(response=>{
+        console.log(response)
+        this._data.access_key=response;
+      })
+    })
+  }
 
   ngOnInit() {
   }
@@ -20,16 +33,17 @@ export class LoginComponent implements OnInit {
   }
   login(){
     // this.http.post('login',this.user).subscribe(response=>console.log(response))
-    var reqHeader = new HttpHeaders({authorization:'Basic '+ btoa('skaterik:pass')});
-    this.http.post('http://localhost:8095/oauth/token?grant_type=client_credentials&username='+this.user.username+'&password='+this.user.password,{"headers":reqHeader})
+    // var reqHeader = new HttpHeaders({authorization:'Basic '+ btoa('skaterik:pass')});
+    
+    var reqHeader = new HttpHeaders({authorization:'Basic c2thdGVyaWs6cGFzcw=='});
+    this.http.post('/oauth/token?grant_type=password&username='+this.user.username+'&password='+this.user.password,{"headers":reqHeader})
     .subscribe(response=>{
       console.log(response)
       this._data.access_key=response;
       this.router.navigateByUrl('legends');
     })
-
-    
   }
-
+  
+  
 }
 
